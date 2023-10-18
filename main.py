da2011486076f21bd274555115ec54eccbf0d1a3
miha- <kondr-mih@mail.ru>
2023-10-18 14:14:36 +0300

    vk-api getting friends done

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import vk_api
 import json
 
+
 TOKEN = ''
+
 
 def get_friends(vk, id, fr_dict: dict):
     curr_friends = fr_dict.keys()
     try:
-        friends = vk.friends.get(user_id=id)['items']
+        friends = vk.friends.get(user_id=id, count=10)['items']
         for friend in curr_friends:
             if friend in friends:
                 fr_dict[id].append(friend)
@@ -15,37 +17,37 @@ def get_friends(vk, id, fr_dict: dict):
         fr_dict.pop(id)
 
 
-# def check_friends(friends, potential_friends):
-#     result = list(map(lambda x: x in friends, potential_friends))
-#     if len(result) != 0:
-#         return True, result
-#     else:
-#         return False, None
-
-
 vk_session = vk_api.VkApi(token=TOKEN)
-# vk_session.auth()
 
 vk = vk_session.get_api()
+id_root = vk.account.getProfileInfo()['id']
 
-friends = vk.friends.get()
+friends = vk.friends.get(count=15)
 
 friends_dict = dict()
+friends_dict[id_root] = []
 
 for id in friends['items']:
-    friends_dict[id] = []
+    try:
+        _ = vk.friends.get(user_id=id, count=1)
+    except vk_api.exceptions.ApiError:
+        continue
+
+    friends_dict[id_root].append(id)
+
+    friends_dict[id] = [id_root]
     get_friends(vk, id, friends_dict)
 
-    # начало для настройки глубины и количества друзей
-    # next_friends = vk.friends.get(user_id=id, count=10)['items']
-    # same, people = check_friends(friends=friends_dict.keys(),
-    #                       potential_friends=next_friends)
-    # if same:
-    #     next_friends.remove(people)
+    next_friends = vk.friends.get(user_id=id, count=15)['items']
+
+    for fr_id in next_friends:
+        friends_dict[id].append(fr_id)
+
+        friends_dict[fr_id] = [id]
+        get_friends(vk, fr_id, friends_dict)
 
 friends_json = json.dumps(friends_dict, indent=4, separators=(',', ': '))
 with open('friends.json', 'w') as file:
     file.write(friends_json)
 
-# print(friends_json)
-# print(len(friends_dict.keys()))
+print(len(friends_dict.keys()))
